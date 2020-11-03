bda39c53b2606a79ab3d5d1f97485300c5ccc7a2
Sehej Jain <Sehejjain@outlook.com>
2020-11-03 20:24:30 +0530

    Home

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="home" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<title>Linker</title>
+</head>
+
+<body>
+	<form action="submitQuestion.php" method="post">
+		Ask a new Question: <br><textarea rows = "5" cols = "50"
+			name = "newquestion"></textarea><br>
+		 <input type="submit" value="Submit" />
+	</form>
+	<iframe src="displayQuestions.php" width="650" height="400"></iframe>
+
+</body>
+
+</html>
+
